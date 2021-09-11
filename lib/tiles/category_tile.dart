402c509ed6750b7b7cb7afd892fe59e331f1ef03
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-09-10 21:45:25 -0300

    Criado a tela para cada categoria de produtos

--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,5 +1,7 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -17,6 +19,11 @@ class CategoryTile extends StatelessWidget {
       ),
       trailing: Icon(Icons.keyboard_arrow_right),
       onTap: (){
+        Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (context) => CategoryScreen(snapshot)
+            )
+        );
 
       },
     );
