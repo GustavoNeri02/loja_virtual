78e147231e40c490f5f29d30d3b3862e447398b8
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-09-19 20:37:05 -0300

    Criando o modelo carrinho

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_virtual/datas/cart_product.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartModel extends Model {
+
+  UserModel user;
+
+  List<CartProduct> products = [];
+
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct){
+    products.add(cartProduct);
+    Firestore.instance.collection("Users").document(user.firebaseUser!.uid)
+        .collection("cart").add(cartProduct.toMap()).then((doc) {
+          cartProduct.cartId = doc.documentID;
+    });
+    notifyListeners();
+  }
+  void removeCartItem(CartProduct cartProduct){
+    products.remove(cartProduct);
+    Firestore.instance.collection("Users").document(user.firebaseUser!.uid)
+        .collection("cart").document(cartProduct.cartId).delete();
+    notifyListeners();
+  }
+}
