d1f86ebe6211e72ab5b01ec9a7f6b7545c548672
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2022-03-04 23:58:46 -0300

    Estruturando as lojas e corrigindo ordem de pedidos em orders_tab.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
 import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/cart_button.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
@@ -38,10 +39,12 @@ class HomeScreen extends StatelessWidget {
           body: OrdersTab(),
         ),
         Scaffold(
-          drawer: CustomDrawer(_pageController),
-          body: Container(
-            color: Colors.yellow,
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
           ),
+          drawer: CustomDrawer(_pageController),
+          body: PlacesTab(),
         ),
       ],
     );
---
+++ lib/tiles/place_tile.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class PlaceTile extends StatelessWidget {
+
+  final DocumentSnapshot documentSnapshot;
+
+  PlaceTile(this.documentSnapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
